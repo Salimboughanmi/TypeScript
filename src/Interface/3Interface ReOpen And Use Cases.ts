9238d07577b651fd5786f6b98e63fbeda7d04fa5
salimboughanmi <boughanmi1salim@gmail.com>
2024-09-08 20:42:42 +0100

    Interface Reopen And Use Cases

--- src/Interface/3Interface ReOpen And Use Cases.ts
+++ src/Interface/3Interface ReOpen And Use Cases.ts
@@ -2,3 +2,26 @@
   Interface
   - ReOpen The Interface And Use Cases
 */
+
+// Homepage
+interface settings {
+  readonly theme: boolean;
+  font: string;
+}
+
+// Articles Page
+interface settings {
+  sidebar: boolean;
+}
+
+// Contact Page
+interface settings {
+  external: boolean;
+}
+
+let usersettings: Settings = {
+  theme: true,
+  font: "Open Sans",
+  sidebar: false,
+  external: true
+}
