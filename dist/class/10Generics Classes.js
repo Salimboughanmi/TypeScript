7063c92f0bc7832c202c03b7ad1bc28b0f384477
salimboughanmi <boughanmi1salim@gmail.com>
2024-09-10 00:55:40 +0100

    Generics And Interfaces

--- dist/class/10Generics Classes.js
+++ dist/class/10Generics Classes.js
@@ -1,2 +1,16 @@
 "use strict";
+class Person {
+    constructor(value) {
+        this.value = value;
+    }
+    show(msg) {
+        console.log(`${msg} - ${this.value}`);
+    }
+}
+let userOne = new Person("Elzero");
+console.log(userOne.value);
+userOne.show("Message");
+let userTwo = new Person(100);
+console.log(userTwo.value);
+userTwo.show("Message");
 //# sourceMappingURL=10Generics%20Classes.js.map
