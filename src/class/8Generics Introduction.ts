a5eed9d87b11e557fe77b11743317a33bdf4bcd5
salimboughanmi <boughanmi1salim@gmail.com>
2024-09-09 22:38:15 +0100

    Generics Introduction

--- src/class/8Generics Introduction.ts
+++ src/class/8Generics Introduction.ts
@@ -9,3 +9,28 @@
   --- Generic Methods
   --- Generic Interfaces
 */
+
+/* function returnNumber(val : number) :number{
+return(val)
+}
+
+function returnString(val : string) :string{
+  return(val)
+  }
+
+  function returnBoolean(val : boolean) :boolean{
+    return(val)
+    }   
+
+console.log(returnNumber(5)); */
+    
+function returnType<T>(val : T) :T{  // ou insteat T remplace par GenericType
+  return(val)
+  }
+
+  console.log(returnType<string>("heloooooooo"));
+  console.log(returnType<number>(16));
+  console.log(returnType<boolean>(true));
+  console.log(returnType<(number|string)[]>([1,3,6,5,65,"uu",6,5]))
+  console.log(returnType<number[]>([1,3,6,5,65,6,5]))
+
