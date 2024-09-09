98e813e151085953b3466dffe7ccc4f4f2c21d66
salimboughanmi <boughanmi1salim@gmail.com>
2024-09-10 00:44:07 +0100

    Generics Classes

--- src/class/10Generics Classes.ts
+++ src/class/10Generics Classes.ts
@@ -2,3 +2,18 @@
   Generics
   - Classes
 */
+
+class Person<T = string> {
+  constructor(public value: T) {}
+  show(msg: T) : void {
+    console.log(`${msg} - ${this.value}`);
+  }
+}
+
+let userOne = new Person<string>("Elzero");
+console.log(userOne.value);
+userOne.show("Message");
+
+let userTwo = new Person<number | string>(100);
+console.log(userTwo.value);
+userTwo.show("Message");
